38392251a7501b42e31745e58ef0da8d4ccff4ce
Jere <jerekoivisto7@gmail.com>
2021-03-31 19:25:55 +0300

    Miekkamekaniikka päähahmolle - Jere

---
+++ Assets/Scripts/MiekkaCombat/PlayerCombat.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+
+    public Transform attackPoint; // Gameobject attackpoint miekkaan että tämä toimii https://www.youtube.com/watch?v=sPiVz1k-fEs
+    public LayerMask enemyLayers;
+
+    public int attackDamage = 40;
+    public float attackRange = 0.5f;
+
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
+
+    void Update()
+    {
+        if(Time.time >= nextAttackTime)
+        {
+            if (Input.GetKey(KeyCode.Mouse1))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+    }
+
+    void Attack()
+    {
+        //Play an attack animation
+        animator.SetTrigger("Attack");
+        //Detect enemies in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        // Damage them
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
