5501a4dae0d42904bb5a12a84b8ceea6dddc340d
Jere <jerekoivisto7@gmail.com>
2021-04-01 11:20:34 +0300

    Miekka mekaniikan viilausta - jere

--- Assets/Scripts/MiekkaCombat/PlayerCombat.cs
+++ Assets/Scripts/MiekkaCombat/PlayerCombat.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.InputSystem;
+﻿using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
@@ -18,7 +15,7 @@ public class PlayerCombat : MonoBehaviour
 
     void Update()
     {
-        if(Time.time >= nextAttackTime)
+        if (Time.time >= nextAttackTime)
         {
             if (Input.GetKey(KeyCode.Mouse1))
             {
