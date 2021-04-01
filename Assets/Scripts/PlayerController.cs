daff30d0330b796114d615359fbe37085876f865
TeemuAit <tee.aittola@gmail.com>
2021-04-01 12:02:04 +0300

    Pelaaja hahmon liikkuminen-Teemu

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D theRB;
+
+    float mx;
+
+    private void Update()
+    {
+        mx = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(mx * moveSpeed, theRB.velocity.y);
+
+        theRB.velocity = movement;
+    }
+
+}
