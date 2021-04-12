6bdeae10898d4f2be82b13a0c337dacb5a175a38
Jere Koivisto <jerekoivisto@kamk.fi>
2021-04-12 19:40:48 +0300

    Charge vihollisen tekemistä, player detected ja liikkuminen -Jere

--- Assets/Scripts/Enemies/BasicEnemyController.cs
+++ Assets/Scripts/Enemies/BasicEnemyController.cs
@@ -36,6 +36,11 @@ public class BasicEnemyController : MonoBehaviour
         whatIsPlayer;
     [SerializeField]
     private Vector2 knockbackSpeed;
+    [SerializeField]
+    private GameObject
+        hitParticle,
+        deathChunkParticle,
+        deathBloodParticle;
 
     private float
         currentHealth,
@@ -143,7 +148,8 @@ public class BasicEnemyController : MonoBehaviour
 
     private void EnterDeadState()
     {
-        //Spawn chunks and blood
+        Instantiate(deathChunkParticle, alive.transform.position, deathChunkParticle.transform.rotation);
+        Instantiate(deathBloodParticle, alive.transform.position, deathBloodParticle.transform.rotation);
         Destroy(gameObject);
     }
 
@@ -162,6 +168,8 @@ public class BasicEnemyController : MonoBehaviour
     private void Damage(float[] attackDetails)
     {
         currentHealth -= attackDetails[0];
+
+        Instantiate(hitParticle, alive.transform.position, Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
 
         if (attackDetails[1] > alive.transform.position.x)
         {
