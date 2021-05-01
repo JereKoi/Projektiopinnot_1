40ce45ccb6a23200a0908573ac29ea032ad1541d
Jere <jerekoivisto7@gmail.com>
2021-05-01 19:53:37 +0300

    Healthpickuppien ohjelmointia -Jere

---
+++ Assets/Scripts/Pickups/HealthPickupScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickupScript : MonoBehaviour
+{
+    public int healthToGive;
+
+    public AudioSource pickupSound;
+
+    public PlayerStats PS;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerControllerV2>() == null)
+        {
+            return;
+        }
+
+        PS.DecreaseHealth(-healthToGive);
+
+        pickupSound.Play();
+
+        gameObject.SetActive(true);
+    }
+}
