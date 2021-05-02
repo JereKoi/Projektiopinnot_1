c2f43ec7021ab3ead442129cc1768dde5dc00f17
Jere <jerekoivisto7@gmail.com>
2021-05-02 16:48:38 +0300

    healthpickupit toimii nyt -Jere

--- Assets/Scripts/Pickups/HealthPickupScript.cs
+++ Assets/Scripts/Pickups/HealthPickupScript.cs
@@ -6,20 +6,29 @@ public class HealthPickupScript : MonoBehaviour
 {
     public int healthToGive;
 
-    public AudioSource pickupSound;
+    //public AudioSource pickupSound;
 
     public PlayerStats PS;
 
+    private void Start()
+    {
+        gameObject.SetActive(true);
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<PlayerControllerV2>() == null)
         {
             return;
         }
-        PS.AddHealth();
-        //PS.DecreaseHealth(-healthToGive);
-        pickupSound.Play();
-
-        gameObject.SetActive(false);
+        else if ((int)PS.currentHealth == PS.maxHealth)
+        {
+            return;
+        }
+        else
+        {
+            PS.AddHealth();
+            //pickupSound.Play();
+            gameObject.SetActive(false);
+        }        
     }
 }
